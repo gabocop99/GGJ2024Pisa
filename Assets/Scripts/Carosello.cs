351521567e8d1dc719f22af4508cc5d4d120b345
Raffaele Ariano <rariano@34bigthings.com>
2024-01-27 00:58:37 +0100

    second commit

---
+++ Assets/Scripts/Carosello.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carosello : MonoBehaviour
+{
+    public List<GameObject> cricetiObjects;
+    public Camera CarouselCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < cricetiObjects.Count; i++)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
