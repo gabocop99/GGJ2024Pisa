646677a86c7b28ca7cac80e57f573304156b347a
Gabriele Coppola <gabocop999@gmail.com>
2024-01-27 02:45:41 +0100

    third commit

---
+++ Assets/Scripts/BackStoryBaloon.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BackStoryBaloon : MonoBehaviour
+{
+    private string _story;
+
+    private void Start()
+    {
+        //gameObject.GetComponent<TextMeshPro>().text = _story;
+    }
+}
---
+++ Assets/Scripts/Criceto.cs
@@ -0,0 +1,29 @@
+using TMPro;
+using UnityEngine;
+using System.Collections;
+using Unity.VisualScripting;
+
+public class Criceto : MonoBehaviour
+{
+    [SerializeField] private string _cricetoBackStory;
+
+
+    private void Start()
+    {
+        //BackStoryBaloon textCanva = FindObjectOfType<BackStoryBaloon>();
+        ////GameObject textCanvas = GameObject.Find("BaloonCriceto");
+        //if (textCanva != null)
+        //{
+        //    textCanva.gameObject.GetComponent<TextMeshPro>().text = _cricetoBackStory;
+        //}
+        GameObject backStory = GameObject.FindGameObjectWithTag("BackStory");
+        if (backStory != null)
+            backStory.gameObject.GetComponentInChildren<TextMeshPro>().text = _cricetoBackStory;
+    }
+
+    public void Die()
+    {
+        //TODO
+    }
+
+}
